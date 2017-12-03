4559df04c769d81048d11a3693807c2d6212afbd
ctylercarrico <christopher.carrico175@topper.wku.edu>
2017-12-03 11:06:56 -0800

    Recovered code
    working

--- student.c
+++ student.c
@@ -10,13 +10,15 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "os-sim.h"
+#include <string.h>
 
-typedef struct  Queue
-{
+
+typedef struct Queue{
+
     pcb_t *head;
     pcb_t *tail;
+
 } Queue;
 
 /*
@@ -29,14 +31,14 @@ typedef struct  Queue
  * for your use.
  */
 
+
 static pcb_t **current;
-static queue *ready; 
 static pthread_mutex_t current_mutex;
-static pthread_mutex_t ready_mutex; 
-static pthread_cond_t idle_cond; 
-static int robin_robin_flag;
-static int run; 
-static int time_slice;  
+static Queue *ready;
+static pthread_mutex_t ready_mutex;
+static pthread_cond_t idle_condition;
+static int round_robin_flag;
+static int cs_runtime = -1;
 
 /*
  * schedule() is your CPU scheduler.  It should perform the following tasks:
@@ -57,42 +59,31 @@ static int time_slice;
 
 static void schedule(unsigned int cpu_id)
 {
-    if(round_robin_flag == 1)
+if (ready-> head == NULL)
     {
-        run = time_slice;
+        context_switch(cpu_id, NULL, cs_runtime);
     }
-    else  
+    else
     {
-        run = -1; 
-    }
-
-
-    if(readyQueue->head == NULL)  
-    {
-        context_switch(cpu_id, NULL, run);  
-    }
-    else  
-    {
-        pthread_mutex_lock(&ready_mutex); 
+        pthread_mutex_lock(&ready_mutex);
         pcb_t *temp = ready->head;
         temp->state = PROCESS_RUNNING;
-        if(ready->head == ready->tail) 
+        if(ready->head == ready->tail)
         {
-            ready->head = NULL;  
+            ready->head = NULL;
             ready->tail = NULL;
         }
         else
         {
-            ready->head = temp->next; 
+            ready->head = temp->next;
         }
         pthread_mutex_lock(&current_mutex);
         current[cpu_id] = temp;
         pthread_mutex_unlock(&current_mutex);
         pthread_mutex_unlock(&ready_mutex);
-        context_switch(cpu_id, temp, run);
+        context_switch(cpu_id, temp, cs_runtime);
     }
 }
-
 
 /*
  * idle() is your idle process.  It is called by the simulator when the idle
@@ -104,9 +95,9 @@ static void schedule(unsigned int cpu_id)
 extern void idle(unsigned int cpu_id)
 {
     pthread_mutex_lock(&current_mutex);
-    while(pthread_cond_wait(&idle_cond, &current_mutex));  
-    pthread_mutex_unlock(&current_mutex);
-    schedule(cpu_id);
+    while(pthread_cond_wait(&idle_condition, &current_mutex));  //wait until the condition variable is released/signaled
+    pthread_mutex_unlock(&current_mutex);  //unlock the current mutex
+    schedule(cpu_id);  //schedule the cpu
 
     /*
      * REMOVE THE LINE BELOW AFTER IMPLEMENTING IDLE()
@@ -129,22 +120,22 @@ extern void idle(unsigned int cpu_id)
  */
 extern void preempt(unsigned int cpu_id)
 {
-    pthread_mutex_lock(&current_mutex); 
-    pthread_mutex_lock(&ready_mutex);  
+    pthread_mutex_lock(&current_mutex);
+    pthread_mutex_lock(&ready_mutex);
     current[cpu_id]->state = PROCESS_READY;
     if(ready->head == NULL)
     {
-        ready->head = current[cpu_id]; 
+        ready->head = current[cpu_id];
         ready->tail = current[cpu_id];
     }
-    else 
+    else
     {
-        ready->tail->next = current[cpu_id]; 
+        ready->tail->next = current[cpu_id];
         ready->tail = current[cpu_id];
     }
     pthread_mutex_unlock(&ready_mutex);
-    pthread_mutex_unlock(&current_mutex);  
-    schedule(cpu_id);  
+    pthread_mutex_unlock(&current_mutex);
+    schedule(cpu_id);
 }
 
 
@@ -157,10 +148,10 @@ extern void preempt(unsigned int cpu_id)
  */
 extern void yield(unsigned int cpu_id)
 {
-    pthread_mutex_lock(&current_mutex);  //create mutex lock for array of processes
-    current[cpu_id]->state = PROCESS_WAITING;  //set process at index cpu_id to waiting
-    pthread_mutex_unlock(&current_mutex);  //end lock
-    schedule(cpu_id);  //call schedule with cpu_id
+    pthread_mutex_lock(&current_mutex);
+    current[cpu_id]->state = PROCESS_WAITING;
+    pthread_mutex_unlock(&current_mutex);
+    schedule(cpu_id);
 }
 
 
@@ -173,8 +164,8 @@ extern void terminate(unsigned int cpu_id)
 {
 
     pthread_mutex_lock(&current_mutex);
-    current[cpu_id]->state = PROCESS_TERMINATED; 
-    pthread_mutex_unlock(&current_mutex); 
+    current[cpu_id]->state = PROCESS_TERMINATED;
+    pthread_mutex_unlock(&current_mutex);
     schedule(cpu_id);
 }
 
@@ -196,20 +187,21 @@ extern void terminate(unsigned int cpu_id)
  */
 extern void wake_up(pcb_t *process)
 {
-    process->state = PROCESS_READY; 
-    pthread_mutex_lock(&ready_mutex); 
-    if(ready->head == NULL)  
+    process->state = PROCESS_READY;
+    pthread_mutex_lock(&ready_mutex);
+
+    if(ready->head == NULL)
     {
         ready->head = process;
         ready->tail = process;
     }
-    else 
+    else
     {
         ready->tail->next = process;
         ready->tail = process;
     }
     pthread_mutex_unlock(&ready_mutex);
-    pthread_cond_signal(&idleCond);
+    pthread_cond_signal(&idle_condition);
 }
 
 
@@ -222,7 +214,7 @@ int main(int argc, char *argv[])
     int cpu_count;
 
     /* Parse command-line arguments */
-    if (!(argc % 2 == 0 && argc < 5))  //check if the amount of arguments is within the range
+    if (!(argc % 2 == 0 && argc < 5))
     {
         fprintf(stderr, "CS 425 Project 3 -- Multithreaded OS Simulator\n"
             "Usage: ./os-sim <# CPUs> [ -r <time slice> ]\n"
@@ -236,18 +228,19 @@ int main(int argc, char *argv[])
     if(argc > 2)
     {
         if(strcmp(argv[2], "-r") == 0)
+
         {
-            round_robin_flag = 1;  
-            time_slice = atoi(argv[3]); 
+            round_robin_flag = 1;
+            cs_runtime = atoi(argv[3]);
         }
     }
 
     /* Allocate the current[] array and its mutex */
     current = malloc(sizeof(pcb_t*) * cpu_count);
-    ready = malloc(sizeof(queue*));  
-        assert(current != NULL);
+    ready = malloc(sizeof(Queue*));
+    assert(current != NULL);
     pthread_mutex_init(&current_mutex, NULL);
-    pthread_mutex_init(&ready_mutex, NULL); 
+    pthread_mutex_init(&ready_mutex, NULL);
 
     /* Start the simulator in the library */
     start_simulator(cpu_count);
